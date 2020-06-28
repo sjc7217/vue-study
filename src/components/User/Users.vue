cb34db740b40a359e7045c2eb0fcd66e637dbe15
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 22:04:19 +0800

    194 operative button

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -38,7 +38,18 @@
                 </el-switch>
             </template>
         </el-table-column>
-        <el-table-column label="操作" ></el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template >
+            <!-- 修改按钮 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
   </div>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Card , Row, Col, Table, TableColumn, Switch} from 'element-ui'
+import { Button, Card , Row, Col, Table, TableColumn, Switch, Tooltip} from 'element-ui'
 import { Form, FormItem, Input, Message, Container, Aside, Header, Main, Menu, MenuItem, Submenu, Breadcrumb, BreadcrumbItem} from 'element-ui'
 
 Vue.use(Button)
@@ -21,4 +21,5 @@ Vue.use(Col)
 Vue.use(Table)
 Vue.use(TableColumn)
 Vue.use(Switch)
+Vue.use(Tooltip)
 Vue.prototype.$message = Message
