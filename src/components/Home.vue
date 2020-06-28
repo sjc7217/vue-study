9de14e6753e9d58878a26fc117fdca9b6b37d661
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 19:26:21 +0800

    177 header layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="home-container">
-    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-header>
+        <div>
+            <img src='../assets/heima.png'>
+            <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
     <el-container>
         <!-- 侧边栏 -->
         <el-aside width="200px">Aside</el-aside>
@@ -28,6 +34,19 @@ export default {
 
 .el-header{
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div{
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
 }
 
 .el-aside{
