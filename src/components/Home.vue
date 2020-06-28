5b6797dcdfea1531550eab81410de0d9c5d7d3a0
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 19:18:14 +0800

    176 main layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-      <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右侧区域 -->
+        <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,5 +22,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container{
+    height: 100%;
+}
 
+.el-header{
+    background-color: #373d41;
+}
+
+.el-aside{
+    background-color: #333744;
+}
+
+.el-main{
+    background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
-import { Form, FormItem, Input, Message } from 'element-ui'
+import { Form, FormItem, Input, Message, Container, Aside, Header, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Main)
 Vue.prototype.$message = Message
