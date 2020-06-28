d1f7d5580fe6592d1fd6a0699a644ff84f48dde2
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 18:17:52 +0800

    117

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,27 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/login', component: Login },
   { path: '/', redirect: '/login' },
-  { path: '/home', component: Home}
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  //to:访问路径
+  //from:从哪个路径跳转而来
+  //next:放行函数
+  if (to.path === '/login') {
+    return next()
+  }
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
