ada4b59ca0a8fdd3549720afb430c3f2d3ee441b
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 19:52:07 +0800

    179 token interceptors

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 //axios配置
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 Vue.prototype.$http = axios
 
 
