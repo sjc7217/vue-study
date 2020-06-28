6ea6ea34c7338f486d12ff3270721101467687a3
Shane Shi <shane_shi@trendmicro.com>
2020-06-28 18:21:40 +0800

    118 logout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Home component</div>
+  <div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        logout: function(){
+            window.sessionStorage.clear()
+            this.$router.push('/login')
+        }
+    }
 }
 </script>
 
