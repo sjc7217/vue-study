cb535734c12cd3d57052c790f52108780fc2b37f
Shane Shi <shane_shi@trendmicro.com>
2020-06-29 22:23:57 +0800

    235 role tree input

--- src/components/Rights/Roles.vue
+++ src/components/Rights/Roles.vue
@@ -39,14 +39,14 @@
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc" ></el-table-column>
         <el-table-column label="操作" width="380px">
-            <template >
+            <template slot-scope="scope">
                 <!-- 修改按钮 -->
                 <el-button type="primary" icon="el-icon-edit" size="mini" >编辑</el-button>
                 <!-- 删除按钮 -->
                 <el-button type="danger" icon="el-icon-delete" size="mini" >删除</el-button>
                 <!-- 分配角色按钮 -->
                 <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
-                <el-button type="warning" icon="el-icon-setting" size="mini" @click="showSetRightDialog">分配权限</el-button>
+                <el-button type="warning" icon="el-icon-setting" size="mini" @click="showSetRightDialog(scope.row)">分配权限</el-button>
                 </el-tooltip>
           </template>
         </el-table-column>
@@ -56,7 +56,7 @@
     <!-- 修改用户权限对话框 -->
     <el-dialog title="分配权限" :visible.sync="setRightDialogVisable" width="50%" >
       <!-- 树形控件 -->
-      <el-tree :data="rightsList" :props="treeProps" ></el-tree>
+      <el-tree :data="rightsList" :props="treeProps" show-checkbox node-key="id" default-expand-all :default-checked-keys="defKeys"></el-tree>
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="setRightDialogVisable = false">取 消</el-button>
@@ -78,7 +78,9 @@ export default {
             treeProps: {
                 children: 'children',
                 label: 'authName'
-            }
+            },
+            //默认勾选数组
+            defKeys:[]
         }
     },
     methods: {
@@ -110,11 +112,21 @@ export default {
             role.children = res.data
         },
         //展示分配权限的对话框
-        async showSetRightDialog(){
+        async showSetRightDialog(role){
             const {data: res} = await this.$http.get('rights/tree')
             this.rightsList = res.data
-            console.log(this.rightsList)
+            //console.log(this.rightsList)
+            this.getLeafKeys(role, this.defKeys)
             this.setRightDialogVisable = true
+        },
+        //获得所有三级节点id
+        getLeafKeys(node, arr){
+            if(!node.children){
+                return arr.push(node.id)
+            }
+            node.children.forEach(item=>{
+                this.getLeafKeys(item, arr)
+            })
         }
 
     },
